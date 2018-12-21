d50769c4526f841da5eac74afee15e06f3487247
AnasAhmed <anas.rezk.js@gmail.com>
2018-12-21 19:06:01 +0200

    setup server configuartion bootstap client part

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # React Redux Server Side Render
+
+### TODO
+
+1. Use Absoulute path
+2. use strict mode https://reactjs.org/docs/strict-mode.html
+3.
--- server/src/client/components/home.js
+++ server/src/client/components/home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>I'm Home component!</div>;
+  return (
+    <div>
+      <div>I'm Home Huge as component!</div>
+      <button onClick={() => console.log('Hello There')}>Click Me :)</button>
+    </div>
+  );
 };
 
 export default Home;
